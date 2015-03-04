50488192493b15a4a14605db01f2824920caf53b
ning.xie <ning.xie@qunar.com>
2015-03-04 09:56:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+## mod-mongodb-notification-broker
+This module is used by Broker daemon to relay **All Notification Log Broks** 
+to a mongoDB database.  
+This module is mainly used to store Notification infos to do some analysis or 
+make reports later.
+
+## SetUp
+Note: Replica set MongoDB instances is the recommended way to work with.  
+
+### Replica Set  
+
+##### MongoDB Replica Set with three instances
+* host1:port1
+* host2:port2
+* host3:port3  
+
+##### MongoDB Replica Set configuration
+* [with authenticate](http://docs.mongodb.org/manual/tutorial/deploy-replica-set-with-auth/)
+* [without authenticate](http://docs.mongodb.org/manual/tutorial/deploy-replica-set/)
+
+##### Configuration in mongodb-broker.cfg
+> module_name     mongodb-notification-broker
+> module_type     mongodb_notification_broker
+> high_availability     true  
+> replica_set       host1:port1, host2:port2, host3:port3  
+> read_preference   secondary
+> database     shinken_broker_notification
+> username     shinken_broker_notification
+> password     shinken_broker_notification
+
+### Stand alone
+
+##### MongoDB Stand alone
+* host:port
+
+##### Configuration in mongodb-broker.cfg
+> module_name     mongodb_notification_broker
+> module_type     mongodb_notification_broker
+> high_availability     false  
+> stand_alone   host:port
+> database     shinken_broker_notification
+> username     shinken_broker_notification
+> password     shinken_broker_notification
---
+++ module/module.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+# 
+# Copyright @ 2015 OPS, Qunar Inc. (qunar.com)
+# Author: ning.xie <andy.xning@qunar.com>
+# 
+
+import sys
+
+try:
+    from pymongo import MongoReplicaSetClient, MongoClient
+    from pymongo.errors import (AutoReconnect, ConnectionFailure, InvalidURI,
+                                ConfigurationError)
+except ImportError:
+    raise Exception('Python binding for MongoDB has not been installed. '
+                    'Please install "pymongo" first')
+    
+from shinken.basemodule import BaseModule
+from shinken.log import logger
+from shinken.util import to_bool
+
+
+properties = {
+              'daemons': ['broker'],
+              'type': 'mongodb-notification-broker',
+              'external': True
+              }
+
+
+# called by the plugin manager to get a mongodb_broker instance
+def get_instance(mod_conf):
+    logger.info('[Mongodb-Notification-Broker] Get a Broker module %s' 
+                % mod_conf.get_name())
+    instance = MongodbBroker(mod_conf)
+    return instance
+
+
+# Main class
+class MongodbBroker(BaseModule):
+    
+    def __init__(self, mod_conf):
+        BaseModule.__init__(self, mod_conf)
+        self._parse_conf(mod_conf)
+        
+        self.conn = None
+        # service notification log broks. 
+        # ref: service.raise_notification_log_entry
+        self.service_notification = ('contact',
+                                     'host',
+                                     'service_description',
+                                     'state',
+                                     'command',
+                                     'output'
+                                     )
+        # host notification log broks.
+        # ref: host.raise_notification_log_entry
+        self.host_notification = ('contact',
+                                  'host',
+                                  'state',
+                                  'command',
+                                  'output')
+        
+        
+    def _parse_conf(self, mod_conf):
+        logger.debug(mod_conf)
+        self.high_availability = to_bool(getattr(mod_conf,
+                                                 'high_availability', 'false'))
+        if not self.high_availability:
+            self.stand_alone = getattr(mod_conf, 'stand_alone', '')
+            if not self.stand_alone:
+                logger.error('[Mongodb-Notification-Broker] Mongodb is '
+                             'configured with high availability be false but '
+                             'stand_alone is not configured')
+                raise Exception('[Mongodb-Notification-Broker] Configuration '
+                                'Error')
+        else:
+            replica_set_str = getattr(mod_conf, 'replica_set', '')
+            self.replica_set = self._get_replica_set(replica_set_str)
+        self.database = getattr(mod_conf,
+                                'database', 'shinken_notification_broker')
+        self.username = getattr(mod_conf,
+                                'username', 'shinken_broker_notification')
+        self.password = getattr(mod_conf,
+                                'password', 'shinken-broker_notification')
+        self.url_options = getattr(mod_conf, 'url_options', '')
+        self.retry_per_log = int(getattr(mod_conf, 'retry_per_log', '50'))
+        
+        
+    def _get_replica_set(self, replica_set_str):
+        raw_members = replica_set_str.split(',')
+        members = []
+        for member in raw_members:
+            members.append(member.strip())
+        return members        
+        
+        
+    def _get_mongodb_url(self):
+        scheme = 'mongodb://'
+        db_and_options = '/%s?%s' % (self.database, self.url_options) 
+        credential = ':'.join((self.username, '%s@' % self.password))
+        if not self.high_availability:
+            address = self.stand_alone
+            mongodb_url = ''.join((scheme, credential, address, db_and_options))
+        else:
+            address = ','.join(self.replica_set)
+            mongodb_url = ''.join((scheme, credential, address, db_and_options))
+        return mongodb_url
+        
+        
+    # Called by Broker to do init work
+    def init(self):
+        logger.info('[Mongodb-Notification-Broker] Initialization of '
+                    'mongodb_notification_broker module')
+        mongodb_url = self._get_mongodb_url()
+        logger.debug('[Mongodb-Notification-Broker] Mongodb connect url: %s' 
+                     % mongodb_url)
+        
+        if self.conn:
+            self.do_stop()
+        try:
+            if not self.high_availability:
+                self.conn = MongoClient(mongodb_url)
+            else:
+                self.conn = MongoReplicaSetClient(mongodb_url)
+        except ConnectionFailure:
+            logger.warn('[Mongodb-Notification-Broker] Can not make connection '
+                        ' with MongoDB')
+            raise
+            
+        except (InvalidURI, ConfigurationError):
+            logger.warn('[Mongodb-Notification-Broker] Mongodb connect url '
+                        'error')
+            logger.warn('[Mongodb-Notification-Broker] Mongodb connect url: %s' 
+                        % mongodb_url)
+            raise 
+        self._get_collections()
+        
+        
+    def _get_collections(self):
+        db = self.conn[self.database]
+        self.hosts = db['hosts']
+        self.services = db['services']
+        self.notifications = db['notifications']
+    
+    
+    # Override the same function in basemodule.py
+    def do_stop(self):
+        self.conn.close()
+        self.conn = None
+    
+    
+    # If we are confronted with AutoReconnect Exception, then we should always 
+    # retry until the operation succeeds. However, if other exception is thrown,
+    # we should ignore the operation and go to next operation.
+    def _process_db_operation(self, operation, *param):
+        reconnect_counter = 0
+        result = None        
+        while True:
+            try:
+                result = operation(*param)
+            except AutoReconnect:
+                reconnect_counter += 1
+                if reconnect_counter % self.retry_per_log == 0 or reconnect_counter == 1:
+                    # avoid to invoke too many write operations
+                    logger.warn('[Mongodb-Notification-Broker] Update error. ' 
+                                'Auto reconnected %d times' % reconnect_counter)
+            except Exception:
+                logger.warn('[Mongodb-Notification-Broker] Update error. '
+                            'operation %s, param %s' % (operation, param))
+                logger.warn(sys.exc_info()[:-1])
+                break
+            else:
+                break
+        return result    
+    
+    
+    # main function to update mongodb database
+    def _save(self, ref, ref_identity, notification):
+        self._process_db_operation(self.notifications.insert, notification)
+        if ref == 'service':
+            _id = '.'.join((ref_identity.get('host'),
+                            ref_identity.get('service_description')))
+            cursor = self._process_db_operation(self.services.find,
+                                                {'_id': _id})
+        elif ref == 'host':
+            _id = ref_identity.get('host')
+            cursor = self._process_db_operation(self.hosts.find, {'_id': _id})
+        
+        if not cursor.count():
+            # if notification insert error, then '_id' will not be in it and we
+            # then should ignore the notification.
+            ref_identity.setdefault('notification_ids',
+                                    [notification.get('_id')] if '_id' in notification else [])
+            ref_identity.setdefault('_id', _id)
+            
+            if ref == 'service':
+                self._process_db_operation(self.services.insert, ref_identity)
+            elif ref == 'host':
+                self._process_db_operation(self.hosts.insert, ref_identity)
+        else:
+            document = cursor[0]
+            notification_ids = document.get('notification_ids')
+            # if notification insert error, then '_id' will not be in it and we 
+            # then should ignore the notification
+            if '_id' in notification:
+                notification_ids.append(notification.get('_id'))
+                if ref == 'service':
+                    self._process_db_operation(self.services.update,
+                                               {'_id': _id},
+                                               {'$set': {'notification_ids': notification_ids}})
+                elif ref == 'host':
+                    self._process_db_operation(self.hosts.update,
+                                               {'_id': _id},
+                                               {'$set': {'notification_ids': notification_ids}})
+    
+    
+    # The main function of mongodb_broker
+    # Override the same function in basemodule.py
+    def do_loop_turn(self):
+        broks = self.to_q.get()
+        for brok in broks:
+            brok.prepare()
+            if brok.type == 'log' and 'NOTIFICATION' in brok.data['log']:
+                msg = brok.data['log']
+                parts = msg.split(':', 1)
+                if 'SERVICE' in parts[0]:
+                    service_identiry, notification = self._process_notification_brok('service',
+                                                                                     self.service_notification,
+                                                                                     parts[1])
+                
+                    self._save('service', service_identiry, notification)
+                elif 'HOST' in parts[0]:
+                    host_identity, notification = self._process_notification_brok('host',
+                                                                                  self.host_notification,
+                                                                                  parts[1])
+                    self._save('host', host_identity, notification)
+
+
+    def _process_notification_brok(self, ref, keys, notification_info):
+        elts = notification_info.split(';', len(keys))
+        info_map = dict(zip(keys, elts))
+        if ref == 'service':
+            ref_identity = {'host': info_map.get('host'),
+                            'service_description': info_map.get('service_description')
+                            }
+        elif ref == 'host':
+            ref_identity = {'host': info_map.get('host')}
+            
+        notification = {'contact': info_map.get('contact'),
+                        'command': info_map.get('command'),
+                        'output': info_map.get('output')
+                        }
+        return ref_identity, notification
+    
+    
+    # invoked by basemodule._main function
+    def main(self):
+        logger.debug('[Mongodb-Notification-Broker] Start main function')
+        while not self.interrupted:
+            self.do_loop_turn()
